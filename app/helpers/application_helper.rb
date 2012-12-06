efcd28b8e52bbfbbfe9a71e2194388ed7b612030
FallenSquirrel <spl.hunter@gmx.net>
2012-12-06 11:45:04 +0100

    Added helper for block syntax, all style sizes now relative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,24 +7,29 @@ module ApplicationHelper
   end
   
   def major_block(title, date=nil, &block)
-    content_tag(:div, :class => "block_wrapper") do
+    content_tag(:div, :class => "major_block_wrapper") do
       concat major_block_headline(title, date)
-      concat capture(&block)
+      concat(content_tag(:div, :class => "major_block_text") do
+        concat capture(&block)
+      end)
     end
   end
   
   def major_block_headline(title, date=nil)
-    content_tag(:div, :class => "block_headline") do
-      concat(content_tag(:span, :class => "block_title") do
+    content_tag(:div, :class => "major_block_headline") do
+      concat(content_tag(:span, :class => "major_block_title") do
         concat title
       end)
       if date then
-        concat(content_tag(:span, :class => "block_date") do
+        concat(content_tag(:span, :class => "major_block_date") do
           concat date
         end)
       end
-      concat(content_tag(:div, :class => "float_clear") do
-      end)
+      concat clear_float
     end
+  end
+  
+  def clear_float()
+    tag(:br, :class => "float_clear")
   end
 end
